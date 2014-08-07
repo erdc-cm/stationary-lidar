ceb886e3d724c40617a9957391e933f3ea9620f4
Tristan Dyer <atdyer@ncsu.edu>
2014-08-07 16:22:25 -0400

    Adds loading a mat file

--- LinescanAnalysis.py
+++ LinescanAnalysis.py
@@ -1,25 +1,37 @@
 import numpy as np
+import scipy as sp
+import scipy.io
 
 
-def readLinescanData():
-    return np.empty((10,10))
+def readLinescanData(laserdatFilename):
+    """
+    This function reads a laserdat.mat file and imports it into python as a _______________
+
+    """
+    return sp.io.loadmat(laserdatFilename)
 
 
 class LinescanAnalysis:
 
     def __init__(self):
-        #This constructor makes a LinescanAnalysis Object
+        """
+        This constructor makes a LinescanAnalysis Object
+        """
         self.scanData=readLinescanData()
         self.ptdat=self.createPtdat()
         self.runup=self.getRunupLine()
 
     def createPtdat(self):
-        #This function creates organizes ptDat from scanData and pre-filters it
-        return 
+        """
+        This function creates organizes ptDat from scanData and pre-filters it
+        """
+        return
 
 
     def getRunupline(self):
-        #This function finds the runup line from ptdat
+        """
+        This function finds the runup line from ptdat
+        """
 
 
         pass
